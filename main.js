c497736a51c7639cd6b20f18486c1cc7480a7b3a
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-03-24 23:52:48 +0000

    make basic functionality

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+const searchBar = document.querySelector("input");
+const searchButton = document.querySelector("button");
+
 const path = "https://www.youtube.com/results?search_query=";
 const search_query = "baby shark";
 
-window.open(`${path}${search_query}`, "_self");
+window.addEventListener("keypress", keyEvent=>{
+    if(keyEvent.key==="Enter") window.open(`${path}${searchBar.value}`);
+})
